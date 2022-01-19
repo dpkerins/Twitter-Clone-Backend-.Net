867e6cf5576dc15b5d33ed1d8b3efadcd3dca485
Douglas <douglaskerins@gmail.com>
2022-01-19 14:39:04 +0000

    adding Tweets Model

---
+++ Twitter-Backend/Controllers/TweetController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace Twitter_ASP
+{
+    public class TweetController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+    }
+}
---
+++ Twitter-Backend/Models/Tweets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Twitter_Backend.Models
+{
+    public class Tweet
+    {
+        public Tweet()
+        {
+            public long Id { get; set; }
+            public string Body { get; set; }
+            public long User_Id { get; set; }
+    }
+    }
+}
